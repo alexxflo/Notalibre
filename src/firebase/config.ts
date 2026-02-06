3d79f8bdba5f56ba5ab40fac1ef11c1658011980
alexx Flores <AlexxF48@gmail.com>
2026-02-06 07:29:05 +0000

    en mi actualizacion no me deja iniciar secion  solucionalo dejalo tal cu

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,9 +1,9 @@
 export const firebaseConfig = {
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID || "salvafans",
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID || "1:1071465839463:web:03f74352ac9695d73315a0",
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY || "AIzaSyDEjmuSke21FhIqGqNn2prMtkFR-5MvRS4",
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN || "salvafans.firebaseapp.com",
-  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID || "G-F889T3E7PV",
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID || "1071465839463",
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET || "salvafans.appspot.com",
+  projectId: "salvafans",
+  appId: "1:1071465839463:web:03f74352ac9695d73315a0",
+  apiKey: "AIzaSyDEjmuSke21FhIqGqNn2prMtkFR-5MvRS4",
+  authDomain: "salvafans.firebaseapp.com",
+  measurementId: "G-F889T3E7PV",
+  messagingSenderId: "1071465839463",
+  storageBucket: "salvafans.appspot.com",
 };
