f52a8ce95fc621121f63c8e2d9f437e6495b1996
alexx Flores <AlexxF48@gmail.com>
2026-02-06 04:51:36 +0000

    // src/lib/firebase.ts (o donde tengas tu config) const firebaseConfig =

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,8 +1,8 @@
 export const firebaseConfig = {
-  "projectId": "studio-8356722446-5a6c6",
-  "appId": "1:894641861323:web:2ac2dea31786d0badae14e",
-  "apiKey": "AIzaSyDWUR7HC6dZp3DcsrnSjz2JPrBCDmM5yCs",
-  "authDomain": "studio-8356722446-5a6c6.firebaseapp.com",
-  "measurementId": "",
-  "messagingSenderId": "894641861323"
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
 };
