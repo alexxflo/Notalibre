16450c96367f018aca36260e0b514a7a0140e457
alexx Flores <AlexxF48@gmail.com>
2026-02-06 07:37:51 +0000

    SIGUE SIN DEJARME ENTRAR DAME LA SOLUCION DEFINITIVA

--- src/firebase/config.ts
+++
@@ -1,9 +0,0 @@
-export const firebaseConfig = {
-  projectId: "salvafans",
-  appId: "1:1071465839463:web:03f74352ac9695d73315a0",
-  apiKey: "AIzaSyDEjmuSke21FhIqGqNn2prMtkFR-5MvRS4",
-  authDomain: "salvafans.firebaseapp.com",
-  measurementId: "G-F889T3E7PV",
-  messagingSenderId: "1071465839463",
-  storageBucket: "salvafans.appspot.com",
-};
--- src/firebase/index.ts
+++ src/firebase/index.ts
@@ -1,9 +1,18 @@
 'use client';
 
-import { firebaseConfig } from '@/firebase/config';
 import { initializeApp, getApps, getApp, FirebaseApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
+
+const firebaseConfig = {
+  projectId: "salvafans",
+  appId: "1:1071465839463:web:03f74352ac9695d73315a0",
+  apiKey: "AIzaSyDEjmuSke21FhIqGqNn2prMtkFR-5MvRS4",
+  authDomain: "salvafans.firebaseapp.com",
+  measurementId: "G-F889T3E7PV",
+  messagingSenderId: "1071465839463",
+  storageBucket: "salvafans.appspot.com",
+};
 
 export function initializeFirebase() {
   if (!getApps().length) {
