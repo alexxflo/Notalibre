2d75662ead3b81c55eda0b2d8ea9a6a00adbcf66
alexx Flores <AlexxF48@gmail.com>
2026-02-06 07:09:42 +0000

    no me deja iniciar secion la pagina principal ya actualizada

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,7 +1,7 @@
 export const firebaseConfig = {
   projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID || "salvafans",
   appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID || "1:1071465839463:web:03f74352ac9695d73315a0",
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY || "your-api-key",
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY || "AIzaSyDEjmuSke21FhIqGqNn2prMtkFR-5MvRS4",
   authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN || "salvafans.firebaseapp.com",
   measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID || "G-F889T3E7PV",
   messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID || "1071465839463",
